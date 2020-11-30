aac1b919286a1c4f9e1825aeb146c06fc87aec5a
Jesse <jesse.jafa@gmail.com>
2020-11-30 14:39:37 +0200

    with-rtk ssr example

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,28 @@
 import { Provider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit'
+
+import clockReducer from '../lib/slices/clockSlice'
+import counterReducer from '../lib/slices/counterSlice'
+import notesReducer from '../lib/slices/notesSlice'
 
 import store from '../store'
 
 const MyApp = ({ Component, pageProps }) => {
+
+    let n = configureStore({
+      reducer: {
+        counter: counterReducer,
+        clock: clockReducer,
+        notes: notesReducer,
+      },
+      devTools: true,
+      preloadedState: pageProps.preloadedState
+  })
+
+  console.log(n)
+
   return (
-    <Provider store={store}>
+    <Provider store={n}>
       <Component {...pageProps} />
     </Provider>
   )
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,19 @@
-import { useDispatch } from 'react-redux'
-
+import { useDispatch, useSelector } from 'react-redux'
+import store from '../store';
 import Clock from '../components/clock'
 import Counter from '../components/counter'
 import { tick } from '../lib/slices/clockSlice'
 import useInterval from '../lib/useInterval'
+import { selectClock } from '../lib/slices/clockSlice';
 
-const IndexPage = () => {
+const IndexPage = (props) => {
+  console.log(props)
+  const clock = useSelector(selectClock);
+  console.log(clock);
   const dispatch = useDispatch()
   // Tick the time every second
   useInterval(() => {
-    dispatch(tick({ light: true, lastUpdate: Date.now() }))
+    dispatch(tick({ light: true, lastUpdate: clock.lastUpdate + 1000 }))
   }, 1000)
 
   return (
@@ -18,6 +22,17 @@ const IndexPage = () => {
       <Counter />
     </>
   )
+}
+
+export function getServerSideProps(ctx) {
+  const { dispatch } = store;
+  dispatch(tick({ light: false, lastUpdate: Date.now() + 10000000}));
+
+  return {
+    props: {
+      preloadedState: store.getState()
+    }
+  }
 }
 
 export default IndexPage
